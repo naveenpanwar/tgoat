71cfdd07bd892f078e53b2ce4dc21b0ae2386ae0
naveen <naveen.panwar27@gmail.com>
2016-05-18 16:09:22 +0530

    Using post function and learning trangulation

--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,7 @@ from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
+from django.template.context_processors import csrf
 
 from lists.views import home_page
 
@@ -14,5 +15,21 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        expected_html = render_to_string('lists/home.html')
+        c = dict()
+        c.update(csrf(request))
+        expected_html = render_to_string('lists/home.html', c)
         self.assertEqual(response.content.decode(), expected_html)
+    
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = "POST"
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
+        
+        self.assertIn("A new list item", response.content.decode())
+        c = dict()
+        c.update(csrf(request))
+        c.update({'new_item_text': "A new list item"})
+        expected_html = render_to_string('lists/home.html', c )
+        self.assertEqual(response.content.decode(), expected_html )
