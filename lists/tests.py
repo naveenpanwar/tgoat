015afd0c1ba559a8c4b8053e8671857b5839408e
naveen <naveen.panwar27@gmail.com>
2016-06-04 14:42:59 +0530

    fixing messed up mixed git version

--- lists/tests.py
+++ lists/tests.py
@@ -4,7 +4,7 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 from django.template.context_processors import csrf
 
-from lists.models import Item
+from lists.models import Item, List
 from lists.views import home_page
 
 # Create your tests here.
@@ -29,24 +29,35 @@ class HomePageTest(TestCase):
         expected_html = render_to_string('lists/home.html', c)
         self.assertEqual(response.content.decode(), expected_html)
     
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
     def test_saving_and_retriving_items(self):
+        list_=List()
+        list_.save()
+
         first_item = Item()
         first_item.text = "The first (ever) list item"
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = "The second list item"
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list.list_)
 
         saved_items = Item.objects.all()
         self.assertEqual( saved_items.count(), 2)
 
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, "The first (ever) list item" )
-        self.assertEqual(second_saved_item.text, "The second list item" )
 
+        self.assertEqual(first_saved_item.text, "The first (ever) list item" )
+        self.assertEqual(first_saved_item.list, list_)
+        self.assertEqual(second_saved_item.text, "The second list item" )
+        self.assertEqual(second_saved_item.list, list_)
+        
 class ListViewTest(TestCase):
     def test_uses_list_template(self):
         response = self.client.get('/lists/the-only-list-in-the-world/')
