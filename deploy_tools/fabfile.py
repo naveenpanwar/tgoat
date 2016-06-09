035e6ad9066e2cd31c2d66ef1ca568d9bce0f4c1
naveen <naveen.panwar27@gmail.com>
2016-06-09 16:39:15 +0530

    making changes in the fabrick script

---
+++ deploy_tools/fabfile.py
@@ -0,0 +1,42 @@
+from fabric.contrib.files import append, exists, sed
+from fabric.api import env, local, run
+import random
+
+REPO_URL = "https://github.com/naveenpanwar/tgoat.git"
+
+def deploy():
+    site_folder = '/home/django'
+    source_folder = site_folder+'/tgoat'
+    _create_directory_structure_if_necessary(site_folder)
+    _get_latest_source(source_folder)
+    _update_settings(source_folder, env.host)
+    _update_virtualenv(source_folder)
+    _update_static_files(source_folder)
+    _update_database(source_folder)
+
+def _create_directory_structure_if_necessary(site_folder):
+    for subfolder in ('static','tgoat'):
+        run('mkdir -p %s%s' % (site_folder, subfolder) )
+
+def _get_latest_source(source_folder):
+    if exists(source_folder+'/.git'):
+        run('cd %s && git fetch' % (source_folder,))
+    else:
+        run('git clone %s %s' % (REPO_URL, source_folder))
+    current_commit = local("git log -n 1 --format=%H", capture=True)
+    run('cd %s && git reset --hard %s' % (source_folder, current_commit))
+
+def _update_settings(source_folder, site_name):
+    settings_path = source_folder + '/superlists/settings.py'
+    sed(settings_path, "DEBUG = True", "DEBUG = False")
+    sed(settings_path,
+            'ALLOWED_HOSTS = +$',
+            'ALLOWED_HOSTS = ["%s"]' % (site_name,)
+            )
+    secret_key_file = source_folder + '/superlists/secret_key.py'
+    if not exists(secret_key_file):
+        chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
+        key = ''.join(random.SystemRandom().choice(chars) for _ in range(50))
+        append(secret_key_file, "SECRET_KEY = '%s'" % (key,))
+    append(settings_path, '\nfrom .secret_key import SECRET_KEY')
+
