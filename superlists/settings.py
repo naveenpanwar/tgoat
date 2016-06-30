0d0d252a05764ef8c44d8eceee59a8f10d158ea3
naveen <naveen.panwar27@gmail.com>
2016-06-30 09:58:29 +0530

    changed some settings for local environment

--- superlists/settings.py
+++ superlists/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#@=uib8rv=%ml6%cn+64@h=$=1v3@$4gjbb%ihql-$_amp=a89'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['staging.dogwiki.in']
+ALLOWED_HOSTS = []
 
 
 # Application definition
