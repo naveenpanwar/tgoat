88d68694c57a5d5fc0ea3069f180949575c28f25
naveen <naveen.panwar27@gmail.com>
2016-07-02 13:55:40 +0530

    rename all id_new_item > id_text and new_item > text

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -7,7 +7,7 @@ class ItemFormTest(TestCase):
         form = ItemForm()
         self.assertIn( 'placeholder="Enter a to-do item"', form.as_p() )
         self.assertIn( 'class="form-control input-lg"', form.as_p() )
-    
+
     def test_form_renders_item_text_input(self):
         form = ItemForm()
         self.fail( form.as_p() )
@@ -16,4 +16,3 @@ class ItemFormTest(TestCase):
         form = ItemForm(data={'text':''})
         self.assertFalse(form.is_valid())
         self.assertEqual(form.errors['text'],[EMPTY_ITEM_ERROR])
-
