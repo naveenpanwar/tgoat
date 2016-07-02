d0da5a6e2b0e0d78a9720eb3ddbe4ca2e45d206d
naveen <naveen.panwar27@gmail.com>
2016-07-02 14:41:34 +0530

    deleted two probing tests from test_forms(unit test) and list_item_validation(functional tests)

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -8,10 +8,6 @@ class ItemFormTest(TestCase):
         self.assertIn( 'placeholder="Enter a to-do item"', form.as_p() )
         self.assertIn( 'class="form-control input-lg"', form.as_p() )
 
-    def test_form_renders_item_text_input(self):
-        form = ItemForm()
-        self.fail( form.as_p() )
-
     def test_form_validation_for_blank_items(self):
         form = ItemForm(data={'text':''})
         self.assertFalse(form.is_valid())
