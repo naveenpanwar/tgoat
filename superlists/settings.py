5d80aaebe1e5ec2a19fe3aa849da9939bd960215
naveen <naveen.panwar27@gmail.com>
2016-08-02 16:25:42 +0530

    some doutfull advance in base.html

--- superlists/settings.py
+++ superlists/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'lists.apps.ListsConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE_CLASSES = [
