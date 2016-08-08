a92b9379c68ed1f52f2cd652b1eb2c9a2c807e13
naveen <naveen.panwar27@gmail.com>
2016-08-08 15:25:33 +0530

    using sinon to test login and other functionality

--- accounts/static/accounts/js/accounts.js
+++ accounts/static/accounts/js/accounts.js
@@ -4,15 +4,23 @@ var login_functions = function() {
       navigator.id.request();
     });
     
-    navigator.id.watch({loggedInUser: user});
-  }
+    navigator.id.watch({
+      loggedInUser: user,
+      onlogin: function ( assertion ) {
+        $.post(
+          urls.login,
+          {assertion: assertion, csrfmiddlewaretoken: token }
+        );
+      }
+    });
+  };
 
   window.Superlists = {
     Accounts: {
       initialize: initialize,
     },
   };
-}
+};
 
 $(document).ready( function() {
   login_functions();
